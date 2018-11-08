b960af2b37687e64b51d3b246d6121702d27197a
Philip Schmidt <philip.n.schmidt@gmail.com>
2018-11-08 10:31:42 +0100

    Timer implementation

---
+++ ATM/ATM/Timer/ITimer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ATM.Timer
+{
+    public interface ITimer
+    {
+        void Start(int time);
+
+        event ElapsedEventHandler Elapsed;
+
+        void TimerElapsed(object sender, ElapsedEventArgs e);
+    }
+}
