f9a9c9823ab8dc0aefb0c4efd7e46eca9227e919
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-11-09 14:55:11 +0100

    Påbegynder Integrations-test part 1

---
+++ ATM/ATM.Tests.Integration/IntegrationTestPart1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Events;
+using ATM;
+using ATM.Logger;
+using ATM.Render;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ATM.Tests.Integration
+{
+    [TestFixture]
+    public class IntegrationTestPart1
+    {
+        private FileLogger logger;
+        private ConsoleRenderer renderer;
+        private IConsoleOutput fakeConsoleOutput;
+        private IFileOutput fakeFileOutput;
+        private TrackData trackData;
+        private SeperationEvent seperationEvent;
+        private TrackEnteredEvent trackEnteredEvent;
+        private TrackLeftEvent trackLeftEvent;
+
+
+
+        [SetUp]
+        public void setup()
+        {
+            //Set up stubs
+            fakeConsoleOutput = Substitute.For<IConsoleOutput>();
+            fakeFileOutput = Substitute.For<IFileOutput>();
+
+            //Set up modules under test
+
+            //Set up already tested modules
+        }
+    }
+}
