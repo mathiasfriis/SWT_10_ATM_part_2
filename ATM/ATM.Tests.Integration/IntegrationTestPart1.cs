9b5d61bafda9999356022296fe647d52513fdcae
mathiasfriis <mathiasfriis1995@hotmail.com>
2018-11-09 15:10:58 +0100

    Laver Setup til Integration Test part 1

--- ATM/ATM.Tests.Integration/IntegrationTestPart1.cs
+++ ATM/ATM.Tests.Integration/IntegrationTestPart1.cs
@@ -9,17 +9,26 @@ using ATM.Logger;
 using ATM.Render;
 using NSubstitute;
 using NUnit.Framework;
+//using ATM.Timer;
 
 namespace ATM.Tests.Integration
 {
     [TestFixture]
     public class IntegrationTestPart1
     {
-        private FileLogger logger;
-        private ConsoleRenderer renderer;
+        //S's - Stubs
         private IConsoleOutput fakeConsoleOutput;
         private IFileOutput fakeFileOutput;
-        private TrackData trackData;
+
+        //X's - Modules under test
+        private FileLogger logger;
+        private ConsoleRenderer renderer;
+        //private Timer timer;
+        
+        //T's - Modules acted upon.
+        private TrackData trackData1;
+        private TrackData trackData2;
+        private List<TrackData> tracks;
         private SeperationEvent seperationEvent;
         private TrackEnteredEvent trackEnteredEvent;
         private TrackLeftEvent trackLeftEvent;
@@ -29,13 +38,26 @@ namespace ATM.Tests.Integration
         [SetUp]
         public void setup()
         {
-            //Set up stubs
+            //Set up S's
             fakeConsoleOutput = Substitute.For<IConsoleOutput>();
             fakeFileOutput = Substitute.For<IFileOutput>();
 
-            //Set up modules under test
+            //Set up T's
+            trackData1 = new TrackData("ABC123",10000,10000,1000,"201811071337000",42,10);
+            trackData1 = new TrackData("XYZ987", 10001, 10001, 1001, "201811071338000", 42, 10);
+            tracks = new List<TrackData>()
+            {
+                trackData1,
+                trackData2
+            };
+            seperationEvent = new SeperationEvent("201811071337000",tracks,true);
+            trackEnteredEvent = new TrackEnteredEvent(trackData1._TimeStamp,trackData1,true);
+            trackLeftEvent = new TrackLeftEvent(trackData1._TimeStamp,trackData1,true);
 
-            //Set up already tested modules
+            //Set up X's
+            logger = new FileLogger();
+            renderer = new ConsoleRenderer();
+            //timer = new Timer();
         }
     }
 }
