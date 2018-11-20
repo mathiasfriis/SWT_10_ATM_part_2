108f47ffcbc4b695b54e636bfd7b81d66774edfa
Dalle <Daniel.toettrup@gmail.com>
2018-11-20 13:04:11 +0100

    Tilføjer tests til IT2

--- ATM/ATM.Tests.Integration/IntegrationTestPart2.cs
+++ ATM/ATM.Tests.Integration/IntegrationTestPart2.cs
@@ -10,6 +10,7 @@ using ATM.Render;
 using NSubstitute;
 using NUnit.Framework;
 using System.Threading;
+using TransponderReceiver;
 
 namespace ATM.Tests.Integration
 {
@@ -20,6 +21,7 @@ namespace ATM.Tests.Integration
         private IConsoleOutput fakeConsoleOutput;
         private IFileOutput fakeFileOutput;
         private IIntervalTimer fakeIntervalTimer;
+        private ITransponderReceiver fakeTransponderReceiver;
 
         //X's - Modules under test
         private TrackData trackData1;
@@ -29,11 +31,11 @@ namespace ATM.Tests.Integration
         private Airspace airspace;
         private TrackEnteredEvent trackEnteredEvent;
         private TrackLeftEvent trackLeftEvent;
-        
+
         //T's - Modules acted upon.
         private ATMclass atmClass;
 
-        
+
         [SetUp]
         public void setup()
         {
@@ -41,10 +43,9 @@ namespace ATM.Tests.Integration
             fakeConsoleOutput = Substitute.For<IConsoleOutput>();
             fakeFileOutput = Substitute.For<IFileOutput>();
             fakeIntervalTimer = Substitute.For<IIntervalTimer>();
-            
+            fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+
             //Set up X's
-            //trackData1 = new TrackData("ABC123", 10000, 10000, 1000, "201811071337000", 42, 10);
-            //trackData2 = new TrackData("XYZ987", 10001, 10001, 1001, "201811071338000", 42, 10);
             tracks = new List<TrackData>()
             {
                 trackData1,
@@ -53,42 +54,94 @@ namespace ATM.Tests.Integration
             //seperationEvent = new SeperationEvent("201811071337000", tracks, true);
             //trackEnteredEvent = new TrackEnteredEvent(trackData1._TimeStamp, trackData1, true);
             //trackLeftEvent = new TrackLeftEvent(trackData1._TimeStamp, trackData1, true);
-            airspace = new Airspace(0, 13000, 0, 13000, 500, 2000);
+            airspace = new Airspace(10000, 90000, 10000, 90000, 500, 20000);
 
             //Set up T's
-            //atmClass = new ATMclass(fakeConsoleOutput, fakeFileOutput, airspace);
+            atmClass = new ATMclass(fakeConsoleOutput, fakeFileOutput, airspace, fakeTransponderReceiver);
 
         }
-        
-        
+
+
         #region TrackData
-        /*
+
         [Test]
-        public void TrackData_AddTrackdata_AddTrackToCurrentTracks()
+        public void ATMclass_TrackData_AddTrackToCurrentTracks()
         {
-            TrackData trackData3 = new TrackData("DEF456", 10002, 10002, 1002, "201811071339000", 0, 0,fakeConsoleOutput);
-            atmClass.AddTrack(trackData3);
-            atmClass.RenderTracks();
+            TrackData trackData3 = new TrackData("DEF456", 10002, 10002, 1002, "201811071339000", 0, 0, fakeConsoleOutput);
+            atmClass.HandleNewTrackData(trackData3);
 
             string expectedString =
                 $"{trackData3._Tag} - ( {trackData3._CurrentXcord}, {trackData3._CurrentYcord}, {trackData3._CurrentZcord}) - Speed: {trackData3._CurrentHorzVel} m/s - Course: {trackData3._CurrentCourse} degrees";
 
             fakeConsoleOutput.Received().Print(Arg.Is<string>(expectedString));
         }
-        */
-        /*
+
+
         [Test]
-        public void TrackData_UpdataTrackData_UpdataTrackInCurrentTracks()
+        public void ATMclass_TrackData_UpdataTrackInCurrentTracks()
         {
-            TrackData trackData3 = new TrackData("DEF456", 10002, 10002, 1002, "20181107133900000", 0, 0, fakeConsoleOutput);
-            TrackData trackData4 = new TrackData("DEF456", 10002, 10002, 1002, "20181107133900000", 0, 0, fakeConsoleOutput);
-            atmClass.AddTrack(trackData3);
-            
-            atmClass.UpdateTrackData(trackData4);
-            atmClass.RenderTracks();
+            TrackData trackData1 = new TrackData("DEF456", 10002, 10002, 1002, "20181107133900000", 0, 0, fakeConsoleOutput);
+            TrackData trackData2 = new TrackData("DEF456", 10002, 10002, 1002, "20181107134000000", 0, 0, fakeConsoleOutput);
+            atmClass.HandleNewTrackData(trackData1);
+            atmClass.HandleNewTrackData(trackData2);
 
             string expectedString =
-                $"{trackData4._Tag} - ( {trackData4._CurrentXcord}, {trackData4._CurrentYcord}, {trackData4._CurrentZcord}) - Speed: {trackData4._CurrentHorzVel} m/s - Course: {trackData4._CurrentCourse} degrees";
+                $"{trackData2._Tag} - ( {trackData2._CurrentXcord}, {trackData2._CurrentYcord}, {trackData2._CurrentZcord}) - Speed: {trackData2._CurrentHorzVel} m/s - Course: {trackData2._CurrentCourse} degrees";
+
+            fakeConsoleOutput.Received().Print(Arg.Is<string>(expectedString));
+
+        }
+
+        #endregion
+
+
+        #region Event
+
+        [Test]
+        public void ATMclass_Event_TrackEnteredAirspaceEvent()
+        {
+            TrackData trackData1 = new TrackData("DEF456", 10002, 10002, 1002, "20181107133900000", 0, 0, fakeConsoleOutput);
+
+            atmClass.HandleNewTrackData(trackData1);
+
+            string expectedString =
+                $"Track entered airspace - Occurencetime: {trackData1._TimeStamp} Involved track: {trackData1._Tag}";
+
+            fakeConsoleOutput.Received().Print(Arg.Is<string>(expectedString));
+
+        }
+        /* TrackLeftAirspace event endnu ikke implementeret i atm
+        [Test]
+        public void ATMclass_Event_TrackleftAirspaceEvent()
+        {
+            TrackData trackData1 = new TrackData("DEF456", 10002, 10002, 1002, "20181107133900000", 0, 0, fakeConsoleOutput);
+
+            atmClass.HandleNewTrackData(trackData1);
+
+            TrackData trackData2 = new TrackData("DEF456", 9000, 900, 400, "20181107134000000", 0, 0, fakeConsoleOutput);
+
+            atmClass.HandleNewTrackData(trackData2);
+
+            string expectedString =
+                $"Track left airspace - Occurencetime: {trackData2._TimeStamp} Involved track: {trackData2._Tag}";
+
+            fakeConsoleOutput.Received().Print(Arg.Is<string>(expectedString));
+
+        }*/
+        /*
+        [Test]
+        public void ATMclass_Event_TwoTracksRaiseASeperationEvent()
+        {
+            TrackData trackData1 = new TrackData("DEF456", 11002, 11002, 1202, "20181107133900000", 0, 0, fakeConsoleOutput);
+
+            atmClass.HandleNewTrackData(trackData1);
+
+            TrackData trackData2 = new TrackData("ABC123", 10002, 10002, 1001, "20181107134000000", 0, 0, fakeConsoleOutput);
+
+            atmClass.HandleNewTrackData(trackData2);
+
+            string expectedString =
+                $"Separation event - Occurencetime: {trackData2._TimeStamp} Involved tracks: {trackData1._Tag}, {trackData2._Tag}";
 
             fakeConsoleOutput.Received().Print(Arg.Is<string>(expectedString));
 
@@ -97,16 +150,29 @@ namespace ATM.Tests.Integration
         #endregion
 
 
-        #region Event
-
-
-
-        #endregion
-
-
         #region Airspace
+        [Test]
+        public void ATMclass_Airspace_NewTrackIsInsideAirspace()
+        {
+            TrackData trackData1 = new TrackData("DEF456", 10002, 10002, 1002, "20181107133900000", 0, 0, fakeConsoleOutput);
 
+            atmClass.HandleNewTrackData(trackData1);
 
+            Assert.That(() => atmClass._currentTracks.Exists(x => x._Tag == trackData1._Tag) == true);
+        }
+        [Test]
+        public void Airspace_HandleNewTrackData_TrackIsNoLongerInsideAirspace()
+        {
+            TrackData trackData1 = new TrackData("DEF456", 10002, 10002, 1002, "20181107133900000", 0, 0, fakeConsoleOutput);
+
+            atmClass.HandleNewTrackData(trackData1);
+
+            TrackData trackData2 = new TrackData("DEF456", 9000, 900, 400, "20181107134000000", 0, 0, fakeConsoleOutput);
+
+            atmClass.HandleNewTrackData(trackData2);
+
+            Assert.That(() => atmClass._currentTracks.Exists(x => x._Tag == trackData2._Tag) == false);
+        }
 
         #endregion
 
